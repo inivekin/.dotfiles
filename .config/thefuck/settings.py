4e85dc12a2116e0172a27ca28cdc0beda1bd75bc
inivekin <inivekin@gmail.com>
2018-03-03 17:44:27 +0800

    added thefuck config

---
+++ .config/thefuck/settings.py
@@ -0,0 +1,23 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# priority = {}
+# no_colors = False
+# debug = False
+# env = {'GIT_TRACE': '1', 'LANG': 'C', 'LC_ALL': 'C'}
+# alter_history = True
+# rules = [<const: All rules enabled>]
+# exclude_rules = []
+# wait_command = 3
+# require_confirmation = True
+# repeat = False
+# wait_slow_command = 15
+# history_limit = None
+# slow_commands = ['lein', 'react-native', 'gradle', './gradlew', 'vagrant']
